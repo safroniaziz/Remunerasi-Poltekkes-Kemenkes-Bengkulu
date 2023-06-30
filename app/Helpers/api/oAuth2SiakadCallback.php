ebd76480bf4313986b070d712d60e0b902124882
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-06-30 16:19:18 +0700

    add login form to siakad

--- app/Helpers/api/oAuth2SiakadCallback.php
+++ app/Helpers/api/oAuth2SiakadCallback.php
@@ -25,5 +25,7 @@ $data = array(
 );
 
 $response = _curl_api($config['url'], json_encode($data));
-echo $response;
+$data = json_decode($response, true); // Memparsing respons JSON menjadi array asosiatif
+$name = $data['data']['name']; // Mengakses nilai "name" dari array $data
+echo $name; // Output: Test Remun
 ?>
