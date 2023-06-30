115ce8a2fabdb1ba64afbde91076c352b4bf10b9
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-06-30 16:10:25 +0700

    add login form to siakad

--- public/Helpers/api/oAuth2Client/oAuth2ClientSiakad.js
+++ public/Helpers/api/oAuth2Client/oAuth2ClientSiakad.js
@@ -10,5 +10,5 @@ var oAuth2Siakad = {
 	'type':'dosen',
 	'jquery':true,	// optional if you have jquery fill false
 	'bootstrap':true,	// optional if you have bootstrap fill false
-	'url_callback': "{{ route('callback') }}",
+	'url_callback':'https://poltekkes.risetsetiawan.org/app/Helpers/api/oAuth2SiakadCallback.php',
 }
