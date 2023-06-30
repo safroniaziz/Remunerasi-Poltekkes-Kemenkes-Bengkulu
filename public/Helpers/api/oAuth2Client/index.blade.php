71f232a28b3488b738f82ea033c74c0df53b76b8
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-06-30 23:48:44 +0700

    new commit

--- public/Helpers/api/oAuth2Client/index.blade.php
+++ public/Helpers/api/oAuth2Client/index.blade.php
@@ -1,4 +1,5 @@
 <!-- oAuth2Siakad By mkapl.com / irfan.inside@gmail.com -->
 <link href="oAuth2ClientSiakad.css" rel="stylesheet">
 <div class="container-fluid" id="oAuth2Siakad"></div>
+<script src="oAuth2ClientSiakad.js"></script>
 <script src="https://siakad.poltekkesbengkulu.ac.id/application/js/oAuth2/"></script>
