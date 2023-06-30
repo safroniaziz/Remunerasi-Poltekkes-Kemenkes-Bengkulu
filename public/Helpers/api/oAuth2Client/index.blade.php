fbff8e40a1eddae843132dac86f153c10645ddc0
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-07-01 00:10:27 +0700

    new commit

--- public/Helpers/api/oAuth2Client/index.blade.php
+++ public/Helpers/api/oAuth2Client/index.blade.php
@@ -3,8 +3,20 @@
 <div class="container-fluid" id="oAuth2Siakad"></div>
 <script src="oAuth2ClientSiakad.js"></script>
 <script src="https://siakad.poltekkesbengkulu.ac.id/application/js/oAuth2/"></script>
+<!-- oAuth2Siakad By mkapl.com / irfan.inside@gmail.com -->
+<link href="oAuth2ClientSiakad.css" rel="stylesheet">
+<div class="container-fluid" id="oAuth2Siakad"></div>
+<script src="oAuth2ClientSiakad.js"></script>
+<script src="https://siakad.poltekkesbengkulu.ac.id/application/js/oAuth2/"></script>
+
 <script>
     if (window !== top) {
-        top.location.href = 'https://poltekkes.risetsetiawan.org/public/home';
+        if (window.location !== window.parent.location) {
+            // Frame dalam mode redirect, tidak perlu melakukan redirect lagi
+            console.log('Frame sudah di-redirect');
+        } else {
+            // Frame belum di-redirect, lakukan redirect ke halaman baru
+            top.location.href = 'https://poltekkes.risetsetiawan.org/public/home';
+        }
     }
 </script>
