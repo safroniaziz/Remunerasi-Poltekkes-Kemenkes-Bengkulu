f4670541bdfb4ee0b9dcfa0ec39115493aeb2dbe
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-07-02 19:01:39 +0700

    new commit

--- app/Helpers/api/oAuth2SiakadCallback.php
+++ app/Helpers/api/oAuth2SiakadCallback.php
@@ -30,13 +30,13 @@ $data = json_decode($response, true); // Memparsing respons JSON menjadi array a
 $name = $data['data']['name']; // Mengakses nilai "name" dari array $data
 
 session_start();
-$data = array([
-	'nama'	=>	$data['data']['name'],
-	'kode'	=>	$data['data']['kode'],
-	'gelar'	=>	$data['data']['gelar'],
-	'namatitle'	=>	$data['data']['nametitle'],
-	'nidn'	=>	$data['data']['nidn'],
-]);
+$data = [
+    'nama'      => $data['data']['name'],
+    'kode'      => $data['data']['kode'],
+    'gelar'     => $data['data']['gelar'],
+    'namatitle' => $data['data']['nametitle'],
+    'nidn'      => $data['data']['nidn'],
+];
 
 echo $data;
 // echo "<script>
