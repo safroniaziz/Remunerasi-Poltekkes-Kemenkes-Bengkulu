ae4d00816d96ff2160334254ebca8b39dd583f08
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-07-02 23:19:52 +0700

    new commit

--- resources/views/backend/dosen/rubriks/r_01_perkuliahan_teoris/index.blade.php
+++ resources/views/backend/dosen/rubriks/r_01_perkuliahan_teoris/index.blade.php
@@ -2,6 +2,9 @@
 @section('subTitle','Data Rubrik 01 Perkuliahan Teori')
 @section('page','Data Rubrik 01 Perkuliahan Teori')
 @section('subPage','Semua Data')
+@section('sidebar')
+    @include('layouts.partials.sidebar_dosen')
+@endsection
 @section('login_as')
     Halaman Dosen
 @endsection
