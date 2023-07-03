07317f5ee517b58b1114066c631284a66028e6e4
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-07-03 10:29:49 +0700

    new commit

--- app/Helpers/api/oAuth2SiakadCallback.php
+++ app/Helpers/api/oAuth2SiakadCallback.php
@@ -29,7 +29,7 @@ $response = _curl_api($config['url'], json_encode($data));
 $data = json_decode($response, true); // Memparsing respons JSON menjadi array asosiatif
 $name = $data['data']['name']; // Mengakses nilai "name" dari array $data
 
-print_r($data['data']['namatitle']);
+print_r($data);
 
 // session_start();
 // $_SESSION['data'] = [
