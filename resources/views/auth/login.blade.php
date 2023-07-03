b8a6b450d85a8435464513d08ff1b9329f264ca7
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-07-03 08:40:00 +0700

    new commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -97,7 +97,7 @@
                             </div>
                             <div class="flex-auto pl-6 pr-6">
                                 <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="w-full block text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                                    Login Menggunakan SIAKAD
+                                    LOGIN DENGAN SIAKAD
                                 </button>
                             </div>
                         </div>
