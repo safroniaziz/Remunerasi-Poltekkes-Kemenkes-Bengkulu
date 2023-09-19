2cde46750fe3fe1cf14466e29809ff37114b2ea0
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-09-19 16:34:51 +0700

    new commit

--- public/Helpers/api/oAuth2Client/oAuth2ClientSiakad.js
+++ public/Helpers/api/oAuth2Client/oAuth2ClientSiakad.js
@@ -10,5 +10,5 @@ var oAuth2Siakad = {
 	'type':'dosen',
 	'jquery':true,	// optional if you have jquery fill false
 	'bootstrap':true,	// optional if you have bootstrap fill false
-	'url_callback':'http://103.144.79.249/app/Helpers/api/oAuth2SiakadCallback.php',
+	'url_callback':'http://localhost/app/Helpers/api/oAuth2SiakadCallback.php',
 }
