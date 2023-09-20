beac976ba78adc226788b09606e98b805a6e50ea
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-09-20 22:01:22 +0700

    new commit

--- resources/views/backend/dosen/laporan/cetak.blade.php
+++ resources/views/backend/dosen/laporan/cetak.blade.php
@@ -17,8 +17,7 @@
     <h1 style="text-align: center; text-transform:uppercase">Laporan Remununerasi <br> {{ $judul }}</h1>
     <!-- ... Isi laporan Anda ... -->
     <table style="width: 100%">
-        {{ $riwayatPoints->riwayatPoints }}
-        {{-- @foreach ($riwayatPoints as $riwayatPoint)
+        @foreach ($riwayatPoints->riwayatPoints as $riwayatPoint)
             <tr>
                 <th>Rubrik {{ $riwayatPoint->nama_rubrik }}</th>
             </tr>
@@ -28,7 +27,7 @@
             <tr>
 
             </tr>
-        @endforeach --}}
+        @endforeach
     </table>
 </body>
 </html>
