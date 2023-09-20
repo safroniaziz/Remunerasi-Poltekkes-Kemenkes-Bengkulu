a03838079705728cdec8661aa9f5b99e5b64557b
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-09-20 21:59:03 +0700

    new commit

--- app/Http/Controllers/LaporanDosenController.php
+++ app/Http/Controllers/LaporanDosenController.php
@@ -18,8 +18,8 @@ class LaporanDosenController extends Controller
             'riwayatPoints' => $riwayatPoints,
             'judul' => $judul,
         ];
+        return $data;
         $pdf = PDF::loadView('backend/dosen/laporan.cetak', $data); // Ganti 'nama_view' dengan nama view Anda
-
         return $pdf->stream('laporan_remun_periode_'.$nama_periode.'.pdf');
     }
 }
