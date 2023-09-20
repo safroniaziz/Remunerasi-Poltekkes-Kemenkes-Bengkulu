0e0d567d3375f3df5e98b7b43fee47247d515230
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-09-20 21:47:44 +0700

    new commit

--- resources/views/backend/dosen/laporan/cetak.blade.php
+++ resources/views/backend/dosen/laporan/cetak.blade.php
@@ -3,10 +3,15 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Laporan Remun</title>
+    <style>
+        body {
+            font-family: sans-serif; /* Menggunakan font sans-serif default */
+        }
+    </style>
 </head>
 <body>
     <h1 style="text-align: center;">Laporan Remununerasi <br> {{ $judul }}</h1>
     <!-- ... Isi laporan Anda ... -->
-    
+
 </body>
 </html>
