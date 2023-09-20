15cce456d1d8547b8698838f2cfabe25a3818d5a
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-09-20 15:39:49 +0700

    new commit

--- resources/views/backend/point_rubrik_dosen/detail.blade.php
+++ resources/views/backend/point_rubrik_dosen/detail.blade.php
@@ -59,7 +59,7 @@
         <div class="col-md-8 col-sm-12 col-xs-12">
             <section class="panel" style="margin-bottom:20px;">
                 <header class="bg-primary" style="color: #ffffff;background-color: #3c8dbc;border-color: #fff000;border-image: none;border-style: solid solid none;border-width: 4px 0px 0;border-radius: 0;font-size: 14px;font-weight: 700;padding: 15px;">
-                    <i class="fa fa-info-circle"></i>&nbsp;Detail Isian Rubrik {{ $rekapPerRubrik->nama_rubrik }}
+                    <i class="fa fa-info-circle"></i>&nbsp;Detail Isian Rubrik 
                 </header>
                 <div class="panel-body" style="border-top: 1px solid #eee; padding:15px; background:white;">
                     <div class="row">
