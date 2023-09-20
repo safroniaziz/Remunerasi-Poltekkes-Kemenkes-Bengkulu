eaecf70ac1efa86bb75c7b831a9a72eb74f150b5
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-09-20 22:58:29 +0700

    new commit

--- resources/views/backend/dosen/laporan/_r04.blade.php
+++ resources/views/backend/dosen/laporan/_r04.blade.php
@@ -6,19 +6,19 @@
         <tr>
             <td colspan="3" style="text-transform: uppercase">Total Point {{ $riwayatPoint->point }}</td>
         </tr>
-    <tr>
-        <th>No</th>
-        <th>Jumlah Mahasiswa </th>
-        <th>Point</th>
-    </tr>
+        <tr>
+            <th>No</th>
+            <th>Jumlah Mahasiswa </th>
+            <th>Point</th>
+        </tr>
     </thead>
     <tbody>
-    @foreach ($borangs as $index => $borang)
-        <tr>
-            <td>{{ $index+1 }}</td>
-            <td>{{ $borang->jumlah_mahasiswa }}</td>
-            <td>{{ $borang->point }}</td>
-        </tr>
-    @endforeach
+        @foreach ($borangs as $index => $borang)
+            <tr>
+                <td>{{ $index+1 }}</td>
+                <td>{{ $borang->jumlah_mahasiswa }}</td>
+                <td>{{ $borang->point }}</td>
+            </tr>
+        @endforeach
     </tbody>
 </table>
