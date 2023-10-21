b99c50a308a195b955150c923ed39b1b1f793a81
agoyunib <agoyunib@gmail.com>
2023-10-21 23:37:01 +0700

    sidebar verifikator

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -192,84 +192,132 @@
 
         <ul class="treeview-menu child-sidebar-menu">
             <li class="{{ set_active(['r_01_perkuliahan_teori']) }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_01_perkuliahan_teori') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-book"></i>
                     <span>Perkuliahan Teori</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_02_perkuliahan_praktikum') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_02_perkuliahan_praktikum') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-book-open-reader"></i>
                     <span>Perkuliahan Praktikum</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_03_membimbing_pencapaian_kompetensi') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_03_membimbing_pencapaian_kompetensi') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-magnifying-glass-chart"></i>
                     <span>Memb. Capaian Kompetensi</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_04_membimbing_pendampingan_ukom') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_04_membimbing_pendampingan_ukom') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-square-poll-vertical"></i>
                     <span>Memb. Pendamping UKOM</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_05_membimbing_praktik_pkk_pbl_klinik') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_05_membimbing_praktik_pkk_pbl_klinik') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-house-medical"></i>
                     <span>Memb. Praktik PKK/PBL Klinik</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_06_menguji_ujian_osca') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_06_menguji_ujian_osca') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-briefcase-medical"></i>
                     <span>Menguji Ujian OSCA</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_07_membimbing_skripsi_lta_la_profesi') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_07_membimbing_skripsi_lta_la_profesi') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-solid fa-address-book"></i>
                     <span>Memb. Skripsi/LTA/LA Profesi</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_08_menguji_seminar_proposal_kti_lta_skripsi') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_08_menguji_seminar_proposal_kti_lta_skripsi') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-book-open-reader"></i>
                     <span>Meng. Sempro. LTA/LA/Skripsi</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_09_menguji_seminar_hasil_kti_lta_skripsi') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_09_menguji_seminar_hasil_kti_lta_skripsi') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-laptop-code"></i>
                     <span>Meng. Semhas. LTA/LA/Skripsi</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_010_menulis_buku_ajar_berisbn') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_010_menulis_buku_ajar_berisbn') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-book-bookmark"></i>
                     <span>Menulis Buku Ajar Ber-ISBN</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_011_mengembangkan_modul_berisbn') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_011_mengembangkan_modul_berisbn') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-book"></i>
                     <span>Mengemb. Modul Ber-ISBN</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_012_membimbing_pkm') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_012_membimbing_pkm') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-print"></i>
                     <span>Membimbing PKM</span>
                 </a>
@@ -290,21 +338,29 @@
 
         <ul class="treeview-menu child-sidebar-menu">
             <li class="{{ set_active('r_013_orasi_ilmiah_narasumber_bidang_ilmu') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_013_orasi_ilmiah_narasumber_bidang_ilmu') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-message"></i>
                     <span>Orasi Ilmiah Narasumber</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_014_karya_inovasi') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_014_karya_inovasi') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-envelope"></i>
                     <span>Karya Inovasi</span>
                 </a>
             </li>
         </ul>
     </li>
-            
+
     <li class="treeview {{ set_active([
             'r_015_menulis_karya_ilmiah_dipublikasikan',
             'r_016_naskah_buku_bahasa_terbit_edar_inter',
@@ -318,21 +374,33 @@
         </a>
         <ul class="treeview-menu child-sidebar-menu">
             <li class="{{ set_active('r_015_menulis_karya_ilmiah_dipublikasikan') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_015_menulis_karya_ilmiah_dipublikasikan') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-solid fa-file-word"></i>
                     <span>Karya Ilmiah Dipublikasikan</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_016_naskah_buku_bahasa_terbit_edar_inter') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_016_naskah_buku_bahasa_terbit_edar_inter') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-folder-open"></i>
                     <span>Naskah Buku Internasional</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_017_naskah_buku_bahasa_terbit_edar_nas') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_017_naskah_buku_bahasa_terbit_edar_nas') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-file-pdf"></i>
                     <span>Naskah Buku Nasional</span>
                 </a>
@@ -363,7 +431,11 @@
             </li>
 
             <li class="{{ set_active('r_019_latih_nyuluh_natar_ceramah_warga') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_019_latih_nyuluh_natar_ceramah_warga') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-magnifying-glass"></i>
                     <span>Latihan Pada Masyarakat</span>
                 </a>
@@ -392,80 +464,121 @@
         </a>
         <ul class="treeview-menu child-sidebar-menu">
             <li class="{{ set_active('r_020_assessor_bkd_lkd') }} ">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_020_assessor_bkd_lkd') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
-                <i class="fa fa-user-group"></i>
-                <span>Assesor BKD/LKD</span>
+                @endif
+                    <i class="fa fa-user-group"></i>
+                    <span>Assesor BKD/LKD</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_021_reviewer_eclere_penelitian_dosen') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_021_reviewer_eclere_penelitian_dosen') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-building-user"></i>
                     <span>Rev. Ethical Clearance Dosen</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_022_reviewer_eclere_penelitian_mhs') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_022_reviewer_eclere_penelitian_mhs') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-user-graduate"></i>
                     <span>Rev. Ethical Clearance Mhs</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_023_auditor_mutu_assessor_akred_internal') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_023_auditor_mutu_assessor_akred_internal') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-user-tie"></i>
                     <span>Auditor Mutu/Akred. Internal</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_024_tim_akred_prodi_dan_direktorat') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_024_tim_akred_prodi_dan_direktorat') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-house-signal"></i>
                     <span>Tim Akred. Prodi & Rektorat</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_025_kepanitiaan_kegiatan_institusi') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_025_kepanitiaan_kegiatan_institusi') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-building-columns"></i>
                     <span>Panitia Kegiatan Institusi</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_026_pengelola_jurnal_buletin') }}">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_026_pengelola_jurnal_buletin') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-file-powerpoint"></i>
                     <span>Pengelola Jurnal/Buletin</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_027_keanggotaan_senat') }}">
-                <a class="bg-danger" style="cursor:not-allowed">
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_027_keanggotaan_senat') }}">
+                @else
+                    <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-users"></i>
                     <span>Keanggotaan Senat</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_028_melaksanakan_pengembangan_diri') }}">
-            
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_028_melaksanakan_pengembangan_diri') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-graduation-cap"></i>
                     <span>Melaks. Pengembangan Diri</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_029_memperoleh_penghargaan') }}">
-            
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_029_memperoleh_penghargaan') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-medal"></i>
                     <span>Memperoleh Penghargaan</span>
                 </a>
             </li>
 
             <li class="{{ set_active('r_030_pengelola_kepk') }}">
-            
+                @if(session()->has('nama_dosen'))
+                    <a href="{{ route('r_030_pengelola_kepk') }}">
+                @else
                     <a class="bg-danger" style="cursor:not-allowed">
+                @endif
                     <i class="fa fa-shield-halved"></i>
                     <span>Pengelola KEPK</span>
                 </a>
