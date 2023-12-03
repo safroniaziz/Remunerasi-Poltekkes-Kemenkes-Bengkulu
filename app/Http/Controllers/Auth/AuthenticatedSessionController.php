168c9cbdd9a8ffc96b4e5fc5697520454c46c278
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-12-03 19:01:22 +0700

    new commit

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -30,7 +30,6 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         $user = Auth::user();
-        dd($user);
         if ($user->hasRole('verifikator')) {
             return redirect()->route('cari_dosen'); // Redirect to the admin dashboard
         }else{
