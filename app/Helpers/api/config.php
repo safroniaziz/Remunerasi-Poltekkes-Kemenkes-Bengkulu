f7fed28011ad6444049b57591ced511a6848d32c
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-12-03 15:34:13 +0700

    new commit

--- app/Helpers/api/config.php
+++ app/Helpers/api/config.php
@@ -1,6 +1,6 @@
 <?php 
 $config = array(
-	'url'=>'http://siakad.poltekkesbengkulu.ac.id/application/api/index.php',
+	'url'=>'http://103.219.248.108/application/api/index.php',
 	'client_id'=>'23060001',
 	'version'=>'1.0',
 	'secret_key'=>'Ijz.8!.eo0J0cu3VrjO7',
