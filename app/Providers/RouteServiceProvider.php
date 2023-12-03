30fa507455e3f01423c32d537b990181b074860c
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-12-03 19:18:09 +0700

    new commit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashboard';
     // public const HOMEOPERATORVERIFIKATOR = '/cari_dosen';
 
     /**
