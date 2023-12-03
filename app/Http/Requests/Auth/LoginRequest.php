e0afa5209c3db4120f2e51708ac340898ae9b6a1
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-12-03 18:59:37 +0700

    new commit

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -51,7 +51,6 @@ class LoginRequest extends FormRequest
      */
     public function authenticate(): void
     {
-        dd('a');
         $this->ensureIsNotRateLimited();
         if (!Auth::attempt($this->only($this->id_type, 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
