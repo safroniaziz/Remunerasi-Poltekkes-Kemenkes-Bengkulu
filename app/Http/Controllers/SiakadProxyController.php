681306e45dc0c54f64dc088bc53f7c449ba0e013
safroniaziz <safroni.aziz@gmail.com>
2026-06-17 13:06:52 +0700

    update proxy

--- app/Http/Controllers/SiakadProxyController.php
+++ app/Http/Controllers/SiakadProxyController.php
@@ -71,6 +71,15 @@ class SiakadProxyController extends Controller
         $body        = substr($raw, $headerSize);
         curl_close($ch);
 
+        // Untuk konten teks (JS/HTML/CSS/JSON), tulis ulang setiap rujukan ke
+        // host SIAKAD agar ikut lewat proxy HTTPS kita. Ini menangani URL yang
+        // ter-hardcode di dalam script widget (mis. //103.219.248.108/...),
+        // yang kalau dibiarkan akan di-upgrade browser ke https dan gagal
+        // (ERR_CONNECTION_RESET) karena server SIAKAD tak punya HTTPS.
+        if ($this->isTextContent($contentType)) {
+            $body = $this->rewriteBody($body);
+        }
+
         $response = response($body, $status);
         $response->header('Content-Type', $contentType ?: 'text/html; charset=utf-8');
 
@@ -106,5 +115,36 @@ class SiakadProxyController extends Controller
         }
 
         return $location;
+    }
+
+    private function isTextContent(?string $contentType): bool
+    {
+        if (! $contentType) {
+            return true; // Banyak endpoint SIAKAD tak mengirim Content-Type; anggap teks.
+        }
+
+        return (bool) preg_match(
+            '#(text/|javascript|json|xml|css|html)#i',
+            $contentType
+        );
+    }
+
+    private function rewriteBody(string $body): string
+    {
+        $host = parse_url(config('services.siakad.target'), PHP_URL_HOST);
+
+        if (! $host) {
+            return $body;
+        }
+
+        $proxyBase = rtrim(url('/siakad-proxy'), '/');
+
+        // Ganti http://host, https://host, dan //host (protocol-relative)
+        // menjadi base proxy kita dalam satu kali jalan.
+        return preg_replace(
+            '#(https?:)?//' . preg_quote($host, '#') . '#i',
+            $proxyBase,
+            $body
+        );
     }
 }
